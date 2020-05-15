58bfbfb2d08b1718f6b5e167e9b188e8204e4e61
David Naughton <nihiliad@gmail.com>
2020-05-15 12:32:41 -0500

    Removes eslint, adds standard and results of `standard --fix`.

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-'use strict';
-const path = require('path');
-const config = require('config');
-const uriPathPrefix = (config.has('uriPathPrefix')) ? config.get('uriPathPrefix') : '/';
-const host = (config.has('node.host')) ? config.get('node.host') : '127.0.0.1';
-const port = (config.has('node.port')) ? config.get('node.port') : 1337;
-const errorLogFile = (config.has('log.error')) ? config.get('log.error') : path.resolve(__dirname, 'log/error.json');
-const redirectLogFile = (config.has('log.redirect')) ? config.get('log.redirect') : path.resolve(__dirname, 'log/redirect.json');
+'use strict'
+const path = require('path')
+const config = require('config')
+const uriPathPrefix = (config.has('uriPathPrefix')) ? config.get('uriPathPrefix') : '/'
+const port = (config.has('node.port')) ? config.get('node.port') : 1337
+const errorLogFile = (config.has('log.error')) ? config.get('log.error') : path.resolve(__dirname, 'log/error.json')
+const redirectLogFile = (config.has('log.redirect')) ? config.get('log.redirect') : path.resolve(__dirname, 'log/redirect.json')
 
-const uriFactoryPlugins = require('@nihiliad/janus-uri-factory-plugins');
+const uriFactoryPlugins = require('@nihiliad/janus-uri-factory-plugins')
 const janus = require('@nihiliad/janus').methods({
   redirectLogEvent (ctx, defaultEvent) {
     ['umnLibAccess', 'umnRole'].map(key => {
-      const lcKey = key.toLowerCase();
+      const lcKey = key.toLowerCase()
       defaultEvent.user[key] = (ctx.request.header[lcKey])
         ? ctx.request.header[lcKey].split(';').filter((i) => i.length > 0)
-        : [];
-    });
-    return defaultEvent;
-  },
-});
+        : []
+    })
+    return defaultEvent
+  }
+})
 
 const app = janus({
   uriFactoryPlugins: uriFactoryPlugins,
@@ -26,16 +25,16 @@ const app = janus({
     name: 'error',
     streams: [{
       level: 'info',
-      path: errorLogFile,
-    }],
+      path: errorLogFile
+    }]
   },
   redirectLog: {
     name: 'redirect',
     streams: [{
       level: 'info',
-      path: redirectLogFile,
-    }],
+      path: redirectLogFile
+    }]
   },
-  uriPathPrefix: uriPathPrefix,
-});
-app.listen(port);
+  uriPathPrefix: uriPathPrefix
+})
+app.listen(port)
